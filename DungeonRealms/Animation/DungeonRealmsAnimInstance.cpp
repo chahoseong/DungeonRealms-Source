9ffd6d950ce4059ec54523beaaffa928463bec83
Cha Hoseong <hoseong@chahoseong.com>
2025-08-06 21:29:48 +0900

    check ground info in anim instance

---
+++ DungeonRealms/Animation/DungeonRealmsAnimInstance.cpp
@@ -0,0 +1,23 @@
+﻿#include "Animation/DungeonRealmsAnimInstance.h"
+#include "Character/DungeonRealmsCharacterMovementComponent.h"
+#include "GameFramework/Character.h"
+
+UDungeonRealmsAnimInstance::UDungeonRealmsAnimInstance(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	
+}
+
+void UDungeonRealmsAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	const ACharacter* OwningCharacter =
+		Cast<ACharacter>(GetOwningActor());
+	if (IsValid(OwningCharacter))
+	{
+		const UDungeonRealmsCharacterMovementComponent* CharacterMovement = CastChecked<UDungeonRealmsCharacterMovementComponent>(OwningCharacter->GetCharacterMovement());
+		const FDungeonRealmsCharacterGroundInfo& GroundInfo = CharacterMovement->GetGroundInfo();
+		GroundDistance = GroundInfo.GroundDistance;
+	}
+}
---
+++ DungeonRealms/Animation/DungeonRealmsAnimInstance.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "Animation/AnimInstance.h"
+#include "DungeonRealmsAnimInstance.generated.h"
+
+UCLASS()
+class DUNGEONREALMS_API UDungeonRealmsAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UDungeonRealmsAnimInstance(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category="Character State Data")
+	float GroundDistance = -1.0f;
+};
