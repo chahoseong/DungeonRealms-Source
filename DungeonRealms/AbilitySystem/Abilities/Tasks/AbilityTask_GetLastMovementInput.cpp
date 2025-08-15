4cee9cbddb4a6778640faa9bfcb3ea667ba364f1
Cha Hoseong <hoseong@chahoseong.com>
2025-08-15 15:33:16 +0900

    get last input ability task

---
+++ DungeonRealms/AbilitySystem/Abilities/Tasks/AbilityTask_GetLastMovementInput.cpp
@@ -0,0 +1,105 @@
+﻿#include "AbilitySystem/Abilities/Tasks/AbilityTask_GetLastMovementInput.h"
+#include "AbilitySystemComponent.h"
+#include "GameFramework/Character.h"
+
+UAbilityTask_GetLastMovementInput* UAbilityTask_GetLastMovementInput::GetLastMovementInput(
+	UGameplayAbility* OwningAbility)
+{
+	UAbilityTask_GetLastMovementInput* Task = NewAbilityTask<UAbilityTask_GetLastMovementInput>(OwningAbility);
+	return Task;
+}
+
+UAbilityTask_GetLastMovementInput::UAbilityTask_GetLastMovementInput(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UAbilityTask_GetLastMovementInput::Activate()
+{
+	if (Ability->GetCurrentActorInfo()->IsLocallyControlled())
+	{
+		SendLastMovementInput();
+	}
+	else
+	{
+		if (AbilitySystemComponent.IsValid())
+		{
+			FSimpleMulticastDelegate& Delegate = AbilitySystemComponent->AbilityReplicatedEventDelegate(
+				ReplicatedEventType,
+				GetAbilitySpecHandle(),
+				GetActivationPredictionKey()
+			);
+			ReplicatedEventDelegate = Delegate.AddUObject(this, &ThisClass::OnLastMovementInputReplicated);
+
+			const bool bIsDataSet = AbilitySystemComponent->CallReplicatedEventDelegateIfSet(
+				ReplicatedEventType,
+				GetAbilitySpecHandle(),
+				GetActivationPredictionKey()
+			);
+			if (!bIsDataSet)
+			{
+				SetWaitingOnRemotePlayerData();
+			}
+		}
+	}
+}
+
+void UAbilityTask_GetLastMovementInput::OnDestroy(bool bInOwnerFinished)
+{
+	if (ReplicatedEventDelegate.IsValid())
+	{
+		FSimpleMulticastDelegate& Delegate = AbilitySystemComponent->AbilityReplicatedEventDelegate(
+			ReplicatedEventType,
+			GetAbilitySpecHandle(),
+			GetActivationPredictionKey()
+		);
+		Delegate.Remove(ReplicatedEventDelegate);
+	}
+	Super::OnDestroy(bInOwnerFinished);
+}
+
+void UAbilityTask_GetLastMovementInput::SendLastMovementInput() const
+{
+	if (AbilitySystemComponent.IsValid())
+	{
+		if (const ACharacter* OwningCharacter = Cast<ACharacter>(GetAvatarActor()))
+		{
+			const FVector LastMovementInput = OwningCharacter->GetLastMovementInputVector();
+			AbilitySystemComponent->ServerSetReplicatedEventWithPayload(
+				ReplicatedEventType,
+				GetAbilitySpecHandle(),
+				GetActivationPredictionKey(),
+				AbilitySystemComponent->ScopedPredictionKey,
+				LastMovementInput
+			);
+
+			if (ShouldBroadcastAbilityTaskDelegates())
+			{
+				OnLastMovementInput.Broadcast(LastMovementInput);
+			}
+		}
+	}
+}
+
+void UAbilityTask_GetLastMovementInput::OnLastMovementInputReplicated()
+{
+	if (AbilitySystemComponent.IsValid())
+	{
+		AbilitySystemComponent->ConsumeGenericReplicatedEvent(
+			ReplicatedEventType,
+			GetAbilitySpecHandle(),
+			GetActivationPredictionKey()
+		);
+
+		const FAbilityReplicatedData Data = AbilitySystemComponent->GetReplicatedDataOfGenericReplicatedEvent(
+			ReplicatedEventType,
+			GetAbilitySpecHandle(),
+			GetActivationPredictionKey()
+		);
+		
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnLastMovementInput.Broadcast(Data.VectorPayload);
+		}
+	}
+}
---
+++ DungeonRealms/AbilitySystem/Abilities/Tasks/AbilityTask_GetLastMovementInput.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "Abilities/Tasks/AbilityTask.h"
+#include "AbilityTask_GetLastMovementInput.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FLastMovementInputDelegete, FVector, LastMovementInput);
+
+UCLASS()
+class DUNGEONREALMS_API UAbilityTask_GetLastMovementInput : public UAbilityTask
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="Ability|Tasks", meta=(HidePin="OwningAbility", DefaultToSelf="OwningAbility", BlueprintInternalUseOnly="TRUE"))
+	static UAbilityTask_GetLastMovementInput* GetLastMovementInput(UGameplayAbility* OwningAbility);
+
+public:
+	UAbilityTask_GetLastMovementInput(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	virtual void Activate() override;
+	virtual void OnDestroy(bool bInOwnerFinished) override;
+	
+	UPROPERTY(BlueprintAssignable)
+	FLastMovementInputDelegete OnLastMovementInput;
+
+private:
+	void SendLastMovementInput() const;
+	
+	UFUNCTION()
+	void OnLastMovementInputReplicated();
+	
+private:
+	EAbilityGenericReplicatedEvent::Type ReplicatedEventType = EAbilityGenericReplicatedEvent::GameCustom1;
+	FDelegateHandle ReplicatedEventDelegate;
+};
