04fdd927675123af95358070fc54817be9e8d202
Cha Hoseong <hoseong@chahoseong.com>
2025-08-18 15:29:24 +0900

    AddAnimMontage equipment action

---
+++ DungeonRealms/Equipment/Actions/EquipmentAction_AddAnimMontage.cpp
@@ -0,0 +1,25 @@
+﻿#include "Equipment/Actions/EquipmentAction_AddAnimMontage.h"
+#include "Equipment/DungeonRealmsEquipmentInstance.h"
+#include "Characters/DungeonRealmsCharacter.h"
+
+void UEquipmentAction_AddAnimMontage::OnEquipped(UDungeonRealmsEquipmentInstance* EquipmentInstance)
+{
+	if (ADungeonRealmsCharacter* DungeonRealmsCharacter = EquipmentInstance->GetPawn<ADungeonRealmsCharacter>())
+	{
+		for (const auto& Pair : AnimMontages)
+		{
+			DungeonRealmsCharacter->AddAnimMontage(Pair.Key, Pair.Value);
+		}
+	}
+}
+
+void UEquipmentAction_AddAnimMontage::OnUnequipped(UDungeonRealmsEquipmentInstance* EquipmentInstance)
+{
+	if (ADungeonRealmsCharacter* DungeonRealmsCharacter = EquipmentInstance->GetPawn<ADungeonRealmsCharacter>())
+	{
+		for (const auto& Pair : AnimMontages)
+		{
+			DungeonRealmsCharacter->RemoveAnimMontage(Pair.Key);
+		}
+	}
+}
---
+++ DungeonRealms/Equipment/Actions/EquipmentAction_AddAnimMontage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "DungeonRealmsEquipmentAction.h"
+#include "GameplayTagContainer.h"
+#include "EquipmentAction_AddAnimMontage.generated.h"
+
+UCLASS(DisplayName="Add AnimMontage")
+class DUNGEONREALMS_API UEquipmentAction_AddAnimMontage : public UDungeonRealmsEquipmentAction
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnEquipped(UDungeonRealmsEquipmentInstance* EquipmentInstance) override;
+	virtual void OnUnequipped(UDungeonRealmsEquipmentInstance* EquipmentInstance) override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	TMap<FGameplayTag, TObjectPtr<UAnimMontage>> AnimMontages;
+};
