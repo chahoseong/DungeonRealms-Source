991743ea1404f4824ea0597d1bc2f3158394672a
Cha Hoseong <hoseong@chahoseong.com>
2025-09-10 17:02:28 +0900

    TryActivateAbilityByCass Task

---
+++ DungeonRealms/AI/BehaviorTree/Tasks/BTTask_TryActivateAbilityByClass.cpp
@@ -0,0 +1,39 @@
+﻿#include "AI/BehaviorTree/Tasks/BTTask_TryActivateAbilityByClass.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "Abilities/GameplayAbility.h"
+
+UBTTask_TryActivateAbilityByClass::UBTTask_TryActivateAbilityByClass()
+{
+	NodeName = TEXT("Try Activate Ability By Class");
+	bNotifyTick = false;
+	bNotifyTaskFinished = false;
+	bCreateNodeInstance = false;
+
+	INIT_TASK_NODE_NOTIFY_FLAGS();
+}
+
+FString UBTTask_TryActivateAbilityByClass::GetStaticDescription() const
+{
+	const FString ClassName = AbilityToActivate ? AbilityToActivate->GetName() : TEXT("");
+	return  FString::Printf(TEXT("Try activate ability by class: %s"), *ClassName);
+}
+
+EBTNodeResult::Type UBTTask_TryActivateAbilityByClass::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	if (!AbilityToActivate)
+	{
+		return EBTNodeResult::Aborted;
+	}
+
+	const AController* Owner = OwnerComp.GetOwner<AController>();
+	UAbilitySystemComponent* OwningAbilitySystem = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Owner->GetPawn());
+	
+	if (!IsValid(OwningAbilitySystem))
+	{
+		return EBTNodeResult::Aborted;
+	}
+
+	const bool bActivated = OwningAbilitySystem->TryActivateAbilityByClass(AbilityToActivate, false);
+	return bActivated ? EBTNodeResult::Succeeded : EBTNodeResult::Failed;
+}
---
+++ DungeonRealms/AI/BehaviorTree/Tasks/BTTask_TryActivateAbilityByClass.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_TryActivateAbilityByClass.generated.h"
+
+class UGameplayAbility;
+/**
+ * 
+ */
+UCLASS()
+class DUNGEONREALMS_API UBTTask_TryActivateAbilityByClass : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_TryActivateAbilityByClass();
+
+	//~Begin UBTTaskNode interface
+	virtual FString GetStaticDescription() const override;
+	//~End UBTTaskNode interface
+	
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<UGameplayAbility> AbilityToActivate;
+};
